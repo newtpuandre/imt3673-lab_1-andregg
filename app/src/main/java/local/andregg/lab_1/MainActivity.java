46c294b94387a617387299e30a085d10edcd3652
newtpuandre <andgun96@gmail.com>
2019-01-30 10:45:55 +0100

    Started Initial work on lab 1

--- app/src/main/java/local/andregg/lab_1/MainActivity.java
+++ app/src/main/java/local/andregg/lab_1/MainActivity.java
@@ -1,13 +1,25 @@
 package local.andregg.lab_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView lblBalance = findViewById(R.id.lbl_balance);
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt((110 - 90) + 1) + 90;
+
+        lblBalance.setText(Integer.toString(randomNum));
+
     }
 }
